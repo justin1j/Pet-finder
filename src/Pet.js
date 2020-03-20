2ff464847858a016587ee9622a5c8b58ff039d06
justin1j <wonjun110@gmail.com>
2020-03-20 14:30:07 -0700

    refactor the code with hooks, added useDropdown component

--- src/Pet.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-export function Pet({ name, animal, breed }) {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, name),
-    React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
-  ])
-}
