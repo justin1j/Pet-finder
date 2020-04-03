bc12804d3d192d4140cd711917b3ec984b5036c3
justin1j <wonjun110@gmail.com>
2020-04-03 16:06:49 -0700

    added Details component, added Carousel

--- src/App.js
+++ src/App.js
@@ -6,17 +6,15 @@ import Details from "./Details";
 
 const App = () => {
   return (
-    <React.StrictMode>
-      <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
-    </React.StrictMode>
+    <div>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
+    </div>
   );
 };
 
